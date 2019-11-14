94ff6057498ab74a98f4b655a1cb273e642e4d61
Akha Magaqana <akham20@gmail.com>
2019-11-14 11:39:50 +0200

    adding more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { BookingsComponent } from './bookings/bookings.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'Home',
+    component: HomePageComponent
+  },
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: 'Events',
+    component: EventsComponent
+  },
+  {
+    path: 'Bookings',
+    component: BookingsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SliderComponent } from './slider/slider.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {
+  MatCardModule,
+  MatDividerModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatOptionModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatIconModule
+} from '@angular/material';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClarityModule } from '@clr/angular';
+import { EventsComponent } from './events/events.component';
+import { BookingsComponent } from './bookings/bookings.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    HomePageComponent,
+    LoginComponent,
+    EventsComponent,
+    BookingsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
